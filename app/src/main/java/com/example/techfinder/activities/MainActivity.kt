0d83fa2dda10241f78dc14dd024c4204c41e1b17
afonsofrancof <afonsofrancof@gmail.com>
2022-01-20 16:04:23 +0000

    Navigation working

--- app/src/main/java/com/example/techfinder/activities/MainActivity.kt
+++ app/src/main/java/com/example/techfinder/activities/MainActivity.kt
@@ -8,18 +8,40 @@ import com.example.techfinder.databinding.ActivityMainBinding
 import com.example.techfinder.viewModels.MainViewModel
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
+import android.util.Log
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import android.view.Menu
+import android.view.MenuInflater
+import androidx.appcompat.widget.TooltipCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.techfinder.R
+import com.google.android.material.navigation.NavigationView
+import android.view.View
+
+import androidx.appcompat.app.ActionBarDrawerToggle
+import android.view.MenuItem
+
+import androidx.annotation.NonNull
+import androidx.core.view.GravityCompat
+import com.example.techfinder.fragments.ShopsFeedFragment
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,NavigationView.OnNavigationItemSelectedListener{
 
     private lateinit var navController: NavController
 
-    private lateinit var toolbar: Toolbar
+    private lateinit var toolbar : Toolbar
+
+    private lateinit var drawerLayout:DrawerLayout
+
+    private lateinit var navigationView:NavigationView
 
     private val viewModel: MainViewModel by lazy {
         ViewModelProvider(this).get(MainViewModel::class.java)
@@ -35,13 +57,40 @@ class MainActivity : AppCompatActivity() {
         val binding =
             DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
 
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+        var toggle = ActionBarDrawerToggle(this,drawerLayout , toolbar,
+        R.string.nav_open,R.string.nav_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-       menuInflater.inflate(com.example.techfinder.R.menu.nav_menu, menu)
-        return super.onCreateOptionsMenu(menu)
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        Log.i("degubthis","perfil");
+        when(item.itemId){
+
+            R.id.perfil -> {
+                navController.navigate(R.id.lojaInfoFragment)
+                drawerLayout.closeDrawer(GravityCompat.START)
+            }
+        }
+        return true
     }
 
 }
--- app/src/main/java/com/example/techfinder/fragments/LojaInfoFragment.kt
+++ app/src/main/java/com/example/techfinder/fragments/LojaInfoFragment.kt
@@ -1,12 +1,31 @@
 package com.example.techfinder.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.techfinder.databinding.FragmentLojaInfoBinding
+import com.example.techfinder.databinding.FragmentShopsFeedBinding
 import com.example.techfinder.viewModels.LojaInfoViewModel
 
 class LojaInfoFragment : Fragment() {
 
+    lateinit var binding: FragmentLojaInfoBinding
+
     private val viewModel : LojaInfoViewModel by lazy {
         ViewModelProvider(this).get(LojaInfoViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLojaInfoBinding.inflate(inflater)
+
+
+        return binding.root
     }
 }
