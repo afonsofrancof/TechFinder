92523605e38030352e7b974c3846edbc2efc7414
João Tiago Alves Fidalgo de Sousa <62015233+tsousa111@users.noreply.github.com>
2022-01-21 18:03:28 +0000

    gets da api da DB

--- app/src/main/java/com/example/techfinder/objects/structures.kt
+++ app/src/main/java/com/example/techfinder/objects/structures.kt
@@ -1,40 +1,51 @@
 package com.example.techfinder.objects
 
 import java.sql.Time
-import java.time.LocalDateTime
+import java.sql.Timestamp
 
 data class User(
     var username: String = "",
     var nome: String = "",
     var email: String = "",
     var password: String = "",
-    var morada: String = ""
+    var morada: String = "",
+    var pfpUrl: String? = ""
 )
+
 
 data class LojaPreview(
     var id: String = "",
     var nome: String = "",
-    var localizacaoX: Float,
-    var localizacaoY: Float,
-    var horarioAbertura: Time? = null,
-    var horarioFecho: Time? = null
+    var coordenadas: Coordenadas,
+    var horario: Horario
 )
 
 data class Loja(
     var id: String = "",
     var nome: String = "",
+    var coordenadas: Coordenadas,
     var horarios: MutableList<Horario> = ArrayList(),
-    var website: String? = null,
-    var morada: String? = null,
     var email: String? = null,
+    var website: String? = null,
     var telefone: String = ""
 )
 
 data class Horario(
+    var dia: Int,
     var horarioAbertura: Time? = null,
-    var horarioFecho: Time? = null,
-    var dia: Int
+    var horarioFecho: Time? = null
+)
 
+data class Coordenadas(
+    var x: Float,
+    var y: Float
+)
+
+data class Comentario(
+    var idLoja: String?,
+    var username: String?,
+    var texto: String,
+    var data: Timestamp
 )
 
 data class Categoria(
--- app/src/main/java/com/example/techfinder/utils/DbAPI.kt
+++ app/src/main/java/com/example/techfinder/utils/DbAPI.kt
@@ -1,16 +1,16 @@
 package com.example.techfinder.utils
 
-import com.example.techfinder.objects.Categoria
-import com.example.techfinder.objects.LojaPreview
-import com.example.techfinder.objects.User
-import java.io.*
+import com.example.techfinder.objects.*
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+import java.io.DataInputStream
+import java.io.DataOutputStream
 import java.net.Socket
 import java.sql.Time
 import java.time.LocalDateTime
 
 class DbAPI {
     companion object {
-
         /*
             Queries:
 
@@ -22,74 +22,148 @@ class DbAPI {
                 -> getLoja (tudo) - apenas uma loja
                 -> getLojaPreview (id, nome, local x y , horario a f) - todas as lojas
                 -> getCategorias
-
              */
-
-        fun getUser(username: String): User? {
-            val client = Socket("10.0.2.2", 8888)
+        fun autenticaUser(username: String, password: String): User? {
+            val client = Socket("192.168.0.107", 8888)
             val dos = DataOutputStream(client.getOutputStream())
-            dos.writeUTF("getUser")
-            dos.writeUTF(username)
-            dos.flush();
             val din = DataInputStream(client.getInputStream())
-            val username: String = din.readUTF();
-            val nome: String = din.readUTF();
-            val password: String = din.readUTF();
-            val morada: String = din.readUTF();
-            val email: String = din.readUTF();
-            var user = User(username, nome, email, password, morada)
+            dos.writeUTF("autenticaUser")
+            dos.writeUTF(username)
+            dos.writeUTF(password)
+            dos.flush();
+            dos.close()
+            val autenticado = din.readBoolean();
+            var user: User? = null
+            if (autenticado) {
+                val username: String = din.readUTF()
+                val nome: String = din.readUTF()
+                val password: String = din.readUTF()
+                val morada: String = din.readUTF()
+                val email: String = din.readUTF()
+                user = User(username, nome, email, password, morada)
+            }
+            din.close()
             return user
+        }
+
+        fun criarConta(
+            username: String,
+            password: String,
+            nome: String,
+            morada: String,
+            email: String
+        ): Boolean {
+            val client = Socket("192.168.0.107", 8888)
+            val dos = DataOutputStream(client.getOutputStream())
+            val din = DataInputStream(client.getInputStream())
+            dos.writeUTF("criarConta")
+            dos.writeUTF(username)
+            dos.writeUTF(password)
+            dos.writeUTF(nome)
+            dos.writeUTF(morada)
+            dos.writeUTF(email)
+            dos.flush()
+            dos.close()
+
+            val usernameDisponivel = din.readBoolean()
+            val emailDisponivel = din.readBoolean()
+            din.close()
+            client.close()
+            return usernameDisponivel && emailDisponivel
         }
 
         fun getLojasPreview(): MutableList<LojaPreview> {
             val client = Socket("10.0.2.2", 8888)
             val dos = DataOutputStream(BufferedOutputStream(client.getOutputStream()))
             val din = DataInputStream(BufferedInputStream(client.getInputStream()))
+            val dia = LocalDateTime.now().dayOfWeek.value;
+
             dos.writeUTF("getLojasPreview")
-            dos.writeInt(LocalDateTime.now().dayOfWeek.value)
+            dos.writeInt(dia)
             dos.flush()
+
             val listLojas: MutableList<LojaPreview> = ArrayList()
-            while(true) {
-                if(!din.readBoolean()) break
+            while (din.readBoolean()) {
                 val id = din.readUTF()
                 val nome = din.readUTF()
-                val localizacaoX = din.readFloat()
-                val localizacaoY = din.readFloat()
+                val coordX = din.readFloat()
+                val coordY = din.readFloat()
+                val coordenadas = Coordenadas(coordX, coordY)
                 var horarioString = din.readUTF()
                 val horarioAbertura = Time.valueOf(horarioString)
                 horarioString = din.readUTF()
                 val horarioFecho = Time.valueOf(horarioString)
+                val horario = Horario(dia, horarioAbertura, horarioFecho)
                 listLojas.add(
                     LojaPreview(
                         id,
                         nome,
-                        localizacaoX,
-                        localizacaoY,
-                        horarioAbertura,
-                        horarioFecho
+                        coordenadas,
+                        horario
                     )
                 )
             }
+            din.close()
+            client.close()
             return listLojas
         }
 
-        fun getCategorias(): MutableList<Categoria>{
-
+        // Nao acabado🤏
+        fun getLoja(idLoja: String): Loja {
             val client = Socket("10.0.2.2", 8888)
             val dos = DataOutputStream(BufferedOutputStream(client.getOutputStream()))
             val din = DataInputStream(BufferedInputStream(client.getInputStream()))
-            dos.writeUTF("getLojasPreview")
+
+            dos.writeUTF("getLoja")
+            dos.writeUTF(idLoja)
+            dos.flush()
+            dos.close()
+
+            val id = din.readUTF();
+            val nome = din.readUTF()
+            val website = din.readUTF()
+            val email = din.readUTF()
+            val telefone = din.readUTF()
+            val coordX = din.readFloat()
+            val coordY = din.readFloat()
+
+            val coordenadas = Coordenadas(coordX, coordY)
+
+            val horarios: MutableList<Horario> = ArrayList()
+            while (din.readBoolean()) {
+                val diaSemana = din.readInt();
+                var horarioString = din.readUTF()
+                val horarioAbertura = Time.valueOf(horarioString)
+                horarioString = din.readUTF()
+                val horarioFecho = Time.valueOf(horarioString)
+                val horario = Horario(diaSemana, horarioAbertura, horarioFecho)
+                horarios.add(horario)
+            }
+            din.close()
+            client.close()
+
+            return Loja(
+                id, nome, coordenadas, horarios, email, website, telefone
+            )
+        }
+
+
+        fun getCategorias(): MutableList<Categoria> {
+            val client = Socket("10.0.2.2", 8888)
+            val dos = DataOutputStream(BufferedOutputStream(client.getOutputStream()))
+            val din = DataInputStream(BufferedInputStream(client.getInputStream()))
+
+            dos.writeUTF("getCategorias")
             dos.flush()
             val listCategorias: MutableList<Categoria> = ArrayList()
 
-            while(true){
-                if(!din.readBoolean()) break
+            while (true) {
+                if (!din.readBoolean()) break
                 val nomeCategoria = din.readUTF();
                 listCategorias.add(Categoria(nomeCategoria))
             }
 
             return listCategorias
         }
-
     }
 }
